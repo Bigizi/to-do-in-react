30f4af0f261529f61335dac459ddc677dcdb48a1
Bigizi <bigizinduwayo@gmail.com>
2023-02-27 17:29:29 +0200

    Add header and input components

---
+++ src/components/InputTodo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Todo() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Add todo..." />
+      </form>
+    </div>
+  );
+}
+
+export default Todo;
